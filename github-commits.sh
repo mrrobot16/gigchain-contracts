1a63b651d2e027d92d3bd558e3f3f6d81b7dec7d
hector <hmgarcia06@gmail.com>
2023-02-21 20:35:00 -0500

    Added github-commits.sh for easier git commits

---
+++ github-commits.sh
@@ -0,0 +1,13 @@
+# Github commits script
+# git add src/modules/organization/OrganizationV0.sol && git commit -m "Created OrganizationV0 contract"
+# git add src/modules/organization/OrganizationV1.sol && git commit -m "Created OrganizationV1 contract"
+# git add src/modules/organization/common/Controllable.sol && git commit -m "Created Controllable common contract"
+# git add src/modules/organization/common/Validations.sol && git commit -m "Created Validations common contract"
+# git add src/modules/organization/models/Payment.sol && git commit -m "Created Payment model contract"
+# git add src/modules/organization/models/Member.sol && git commit -m "Created Member model contract"
+# git add src/modules/proxy/OrganizationProxyAdmin.sol && git commit -m "Created ProxyAdmin contract"
+# git add package.json && git commit -m "Added package.json for an easier way to call scripts"
+git add script/deploy.sh && git commit -m "Created script for contract deployment"
+git add remappings.txt && git commit -m "Created remappings file"
+git add github-commits.sh && git commit -m "Added github-commits.sh for easier git commits"
+
