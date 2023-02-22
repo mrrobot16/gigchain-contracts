060d97f2bb9cf346cbfbc8bc333a77bcb46344b9
hector <hmgarcia06@gmail.com>
2023-02-21 20:30:38 -0500

    Created ProxyAdmin contract

---
+++ src/modules/proxy/OrganizationProxyAdmin.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+// TODO: Need to install openzeppelin contracts
+// import "@openzeppelin/contracts/proxy/ProxyAdmin.sol";
+
+contract ProxyAdmin {}
+contract OrganizationProxyAdmin is ProxyAdmin {
+  
+}
